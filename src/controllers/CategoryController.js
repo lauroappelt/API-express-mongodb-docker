22828b87115da88ab46577c11fcfc5278cbe952a
root <lauro@solis.com.br>
2022-01-30 14:29:04 -0300

    Criação dos métodos index, update e delete no categorycontroller.

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -1,6 +1,17 @@
 import Category from "../models/Category";
 
 class CategoryController {
+
+    async index(req, res) {
+        const categories = await Category.find();
+        
+        if (!categories) {
+            return res.status(404).json({error: "category not found"});
+        }
+
+        return res.json(categories);
+    }
+
     async store(req, res){
         const {name} = req.body;
 
@@ -8,7 +19,35 @@ class CategoryController {
             name: name
         });
 
-        return res.json(category);
+        return res.status(201).json(category);
+    }
+
+    async update (req, res) {
+        const {id} = req.params;
+        const {name} = req.body;
+
+        const category = await Category.findById(id);
+        if (!category) {
+            return res.status(404).json({error: "category not found"});
+        }
+
+        category.name = name;
+        category.save();
+
+        return res.status(200).json(category);
+    }
+
+    async delete(req, res) {
+        const {id} = req.params;
+
+        const category = await Category.findById(id);
+        if (!category) {
+            return res.status(404).json({error: "category not found"});
+        }
+
+        category.remove();
+
+        return res.status(200).json({message: "Deleted"});
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,9 @@ import Router from 'express';
 import CategoryController from './controllers/CategoryController';
 const routes = new Router();
 
+routes.get('/categories', CategoryController.index);
 routes.post('/categories', CategoryController.store);
+routes.put('/categories/:id', CategoryController.update);
+routes.delete('/categories/:id', CategoryController.delete);
 
 export default routes;
